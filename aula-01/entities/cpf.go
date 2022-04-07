51de79ec842ffab98ffb946895bf1a07b8a0e570
Bruno Damasceno <damascenobdm@gmail.com>
2022-04-06 22:49:09 -0300

    feat: add validation of equal chars

--- aula-01/entities/cpf.go
+++ aula-01/entities/cpf.go
@@ -1,17 +1,60 @@
 package entities
 
+import (
+	"fmt"
+	"regexp"
+)
+
 type Cpf struct {
 	Value string
 }
 
-func NewCpf(value string) *Cpf {
-	return &Cpf{
-		Value: value,
+func NewCpf(value string) (*Cpf, error) {
+	cpf := Cpf{}
+	cleanCpf, err := cpf.cleanCpf(value)
+	if err != nil {
+		return nil, err
 	}
+
+	err = cpf.validate(cleanCpf)
+	if err != nil {
+		return nil, err
+	}
+	cpf.Value = cleanCpf
+	return &cpf, nil
 }
 
+func (cpf *Cpf) validate(value string) error {
+	size := len([]rune(value))
+	if size != 11 {
+		return fmt.Errorf("invalid cpf size")
+	}
+	err := cpf.checkIfDigitsAreAllEqual(value)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
-// func (cpf *Cpf) validate(value string) {
+func (cpf *Cpf) checkIfDigitsAreAllEqual(value string) error {
+	stringArray := []rune(value)
+	count := 0
+	for i := 0; i < len(stringArray)-1; i++ {
+		if stringArray[i] == stringArray[i+1] {
+			count++
+		}
+	}
+	fmt.Println(count == len(stringArray)-1)
+	if count == len(stringArray)-1 {
+		return fmt.Errorf("invalid cpf")
+	}
+	return nil
+}
 
-// }
-
+func (cpf *Cpf) cleanCpf(value string) (string, error) {
+	regex, err := regexp.Compile(`[^\w]`)
+	if err != nil {
+		return "", err
+	}
+	return regex.ReplaceAllString(value, ""), nil
+}
--- aula-01/entities/cpf_test.go
+++ aula-01/entities/cpf_test.go
@@ -7,7 +7,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestShouldValidateCpf(t *testing.T) {
-	cpf :=entities.NewCpf("123")
-	require.Equal(t, cpf.Value, "123")
+func TestShouldValidateCpfLength(t *testing.T) {
+	_, err := entities.NewCpf("039.187.211-7512")
+	require.Error(t, err)
+}
+
+func TestShouldValidateIfCpfHasAllDigitsEqual(t *testing.T) {
+	_, err := entities.NewCpf("111.111.111-11")
+	require.Error(t, err)
 }
